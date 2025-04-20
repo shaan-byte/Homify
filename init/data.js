0fb1a2037735fe30c847b0ea154248958ffe48b3
shaan <shaanq420@gmail.com>
2025-04-21 00:19:06 +0530

    styling cards and navbar footer

--- init/data.js
+++ init/data.js
@@ -60,18 +60,6 @@ const sampleListings = [
       country: "United States",
     },
     {
-      title: "Beachfront Paradise",
-      description:
-        "Step out of your door onto the sandy beach. This beachfront condo offers the ultimate relaxation.",
-      image: {
-        filename: "listingimage",
-        url: "https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGhvdGVsc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
-      },
-      price: 2000,
-      location: "Cancun",
-      country: "Mexico",
-    },
-    {
       title: "Rustic Cabin by the Lake",
       description:
         "Spend your days fishing and kayaking on the serene lake. This cozy cabin is perfect for outdoor enthusiasts.",
