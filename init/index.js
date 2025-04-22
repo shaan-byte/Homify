a186c63ff678cf545413777ba960264755100abc
Yuval-Varke <varke.vishu01@gmail.com>
2025-04-22 12:29:46 +0530

    image multer done

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb+srv://shaanqureshi770:sara786@shaandb.mibdl85.mongodb.net/Wanderhome"
+const MONGO_URL = "kya be chintu ?"
 
 main()
   .then(() => {
