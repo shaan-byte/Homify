1a82644a0e41d030f5ba66864260fac938405338
shaan <shaanq420@gmail.com>
2025-04-28 17:51:02 +0530

    map fixed

--- public/js/map.js
+++ public/js/map.js
@@ -1,12 +1,25 @@
+mapboxgl.accessToken = mapToken;
 
-  mapboxgl.accessToken = mapToken;
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        style: 'mapbox://styles/mapbox/streets-v11', // style URL
-        center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
-        zoom: 9 // starting zoom
+console.log('Listing coordinates:', listing.geometry.coordinates); // Debug: check order
+
+const coords = listing.geometry.coordinates; // Use as-is
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: coords,
+    zoom: 12
+});
+
+const marker1 = new mapboxgl.Marker({color: "orange"})
+    .setLngLat(coords)
+    .addTo(map);
+
+map.on('load', () => {
+    map.resize();
+    map.flyTo({
+        center: coords,
+        zoom: 12,
+        essential: true
     });
-
-    const marker1 = new mapboxgl.Marker({color:"orange"})
-        .setLngLat(listing.geometry.coordinates)   //listing coordinates
-        .addTo(map);
+});
